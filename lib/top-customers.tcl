fd1f3d4e097eea65ce8117098dc702164d690455
Frank Bergmann <frank.bergmann@project-open.com>
2014-06-08 14:56:19 +0000

    - Top Customers:   Removed unnecessary code

--- lib/top-customers.tcl
+++ lib/top-customers.tcl
@@ -29,38 +29,3 @@ set diagram_id "top_customers_$diagram_rand"
 
 set default_currency [ad_parameter -package_id [im_package_cost_id] "DefaultCurrency" "" "EUR"]
 
-
-# ----------------------------------------------------
-# Create a "multirow" to show the results
-# ----------------------------------------------------
-
-set top_customers_sql "
-	select	company_name as customer_name,
-		sum(c.amount * im_exchange_rate(c.effective_date::date, c.currency, :default_currency)) as customer_revenues
-	from	im_companies cust,
-		im_costs c
-	where	c.customer_id = cust.company_id and
-		c.cost_type_id = [im_cost_type_invoice] and
-		cust.company_path != 'internal'
-	group by company_name
-	order by customer_revenues DESC
-"
-
-multirow create top_customers customer_name customer_revenues
-
-set count 0
-set other_revenues 0.0
-db_foreach top_customers $top_customers_sql {
-    set customer_name_limited [string range $customer_name 0 $max_length_customer_name]
-    if {$customer_name_limited != $customer_name} { append customer_name_limited "..." }
-
-    if {$count < $diagram_max_customers} {
-	multirow append top_customers $customer_name_limited $customer_revenues
-    } else {
-	set other_revenues [expr $other_revenues + $customer_revenues]
-    }
-
-    incr count
-}
-set other_l10n [lang::message::lookup "" intranet-core.Other "Other"]
-multirow append top_customers $other_l10n $other_revenues
