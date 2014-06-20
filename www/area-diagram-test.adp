c835d4d585e48c720f877ddd66778d5f7757450b
Frank Bergmann <frank.bergmann@project-open.com>
2014-06-20 12:20:45 +0000

    - Working on project EVA

---
+++ www/area-diagram-test.adp
@@ -0,0 +1,151 @@
+<master>
+<link rel='stylesheet' href='/sencha-extjs-v421/resources/css/ext-all.css'  type='text/css' media='screen'>
+<script type='text/javascript' src='/sencha-extjs-v421/ext-all.js'></script>
+<div id=diagram_12345></div>
+<script type='text/javascript'>
+
+Ext.require(['Ext.chart.*', 'Ext.Window', 'Ext.fx.target.Sprite', 'Ext.layout.container.Fit']);
+Ext.onReady(function () {
+    
+	projectEvaStore = Ext.create('Ext.data.Store', {
+		fields: ['date', 'planned_value', 'total_planned_value', 'cost_type_3700', 'cost_type_3702', 'cost_type_3703', 'cost_type_3718'],
+		data: [
+	{'date': '2014-06-16 09:00:00', 'planned_value': 0.0, 'total_planned_value': 80.0, 'cost_type_3700': 0.0, 'cost_type_3702': 0.0, 'cost_type_3703': 0.0, 'cost_type_3718': 0.0},
+	{'date': '2014-06-18 09:00:00', 'planned_value': 0.0, 'total_planned_value': 80.0, 'cost_type_3700': 0.0, 'cost_type_3702': 0.0, 'cost_type_3703': 0.0, 'cost_type_3718': 510.0},
+	{'date': '2014-06-19 19:00:00', 'planned_value': 16.0, 'total_planned_value': 80.0, 'cost_type_3700': 0.0, 'cost_type_3702': 0.0, 'cost_type_3703': 0.0, 'cost_type_3718': 270.0},
+	{'date': '2014-06-23 09:00:00', 'planned_value': 16.0, 'total_planned_value': 80.0, 'cost_type_3700': 0.0, 'cost_type_3702': 0.0, 'cost_type_3703': 0.0, 'cost_type_3718': 0.0}
+		       ]
+	    });
+
+	projectEvaChart = new Ext.chart.Chart({
+		animate: false,
+		store: projectEvaStore,
+		legend: { position: 'right' },
+		insetPadding: 20,
+		theme: 'Base:gradients',
+		axes: [{
+			type: 'Numeric',
+			minimum: 0,
+			position: 'left',
+			fields: [
+				 'planned_value', 
+				 'cost_type_3700', 
+				 'cost_type_3702', 
+				 'cost_type_3703', 
+				 'cost_type_3718'
+				 ]
+		    }, {
+			type: 'Time',
+			position: 'bottom',
+			fields: 'date',
+			dateFormat: 'M d'
+		    }],
+		series: [{
+			type: 'area',
+			axis: 'left',
+			xField: 'date',
+			yField: 'planned_value',
+			highlight: true
+		    }, {
+			// Planned Value Series
+			title: 'axis_title_planned_value_l10n',
+			type: 'line',
+			axis: 'left',
+			xField: 'date',
+			yField: 'planned_value',
+			showMarkers: false,
+			tips: {
+			    trackMouse: false,
+			    renderer: function(storeItem, item) {
+				this.setTitle(storeItem.get('date'));
+				this.update(storeItem.get('planned_value'));
+			    }
+			},
+			style: {
+			    fill: '#38B8BF',
+			    stroke: '#38B8BF',
+			    'stroke-width': 3
+			},
+			markerConfig: {
+			    type: 'circle',
+			    size: 4,
+			    radius: 4,
+			    'stroke-width': 0,
+			    fill: '#38B8BF',
+			    stroke: '#38B8BF'
+			}
+		    }, {
+			// Horizontal line for total planned value
+			type: 'line',
+			title: 'axis_title_total_planned_value_l10n',
+			axis: 'left',
+			xField: 'date',
+			yField: 'total_planned_value',
+			showMarkers: false,
+			tips: {
+			    trackMouse: false,
+			    renderer: function(storeItem, item) {
+				this.setTitle(storeItem.get('date'));
+				this.update(storeItem.get('total_planned_value'));
+			    }
+			},
+			style: {
+			    fill: '#FF0000',
+			    stroke: '#FF0000',
+			    'stroke-width': 1
+			}
+		    }, {
+			// Timesheet Cost
+			type: 'line',
+			title: 'axis_title_timesheet_cost_l10n',
+			axis: 'left',
+			xField: 'date',
+			yField: 'cost_type_3718',
+			showMarkers: false,
+			tips: {
+			    trackMouse: false,
+			    renderer: function(storeItem, item) {
+				this.setTitle(storeItem.get('date'));
+				this.update(storeItem.get('total_planned_value'));
+			    }
+			},
+			style: {
+			    fill: '#FF0000',
+			    stroke: '#FF0000',
+			    'stroke-width': 1
+			}
+		    }]
+	    });
+
+
+	var projectEvaPanel = Ext.create('widget.panel', {
+		width: 1000,
+		height: 500,
+		title: 'Test',
+		renderTo: 'diagram_12345',
+		layout: 'fit',
+		header: false,
+		tbar: [{
+			xtype: 'combo',
+			editable: false,
+			store: false,
+			mode: 'local',
+			displayField: 'display',
+			valueField: 'value',
+			triggerAction: 'all',
+			width: 150,
+			forceSelection: true,
+			value: 'all_time',
+			listeners:{select:{fn:function(combo, comboValues) {
+				    var value = comboValues[0].data.value;
+				    var extraParams = projectEvaStore.getProxy().extraParams;
+				    extraParams.diagram_interval = value;
+				    projectEvaStore.load();
+				}
+			    }
+			}
+		    }],
+		items: projectEvaChart
+	    });
+    });
+</script>
---
+++ www/project-eva.json.tcl
@@ -0,0 +1,205 @@
+# /packages/intranet-reporting-dashboard/www/project-eva.tcl
+#
+# Copyright (C) 2014 ]project-open[
+#
+# All rights reserved. Please check
+# http://www.project-open.com/license/ for details.
+
+ad_page_contract {
+    Datasource for top-customers Sencha pie chart.
+} {
+    diagram_project_id:integer
+}
+
+# ----------------------------------------------------
+# Defaults & Permissions
+# ----------------------------------------------------
+
+set current_user_id [ad_get_user_id]
+im_project_permissions $current_user_id $diagram_project_id view read write admin
+if {!$read} {
+    set json "{\"success\": false, \"message\": \"Insufficient permissions - you need read permissions for project #$diagram_project_id.\" }"
+    doc_return 200 "text/html" $json
+    ad_script_abort
+}
+
+set default_currency [ad_parameter -package_id [im_package_cost_id] "DefaultCurrency" "" "EUR"]
+
+# ----------------------------------------------------
+# Calculate diagram time points
+# ----------------------------------------------------
+
+# We need to mix SQL with TCL in order to create an efficient algorithm:
+# We extract all start- and end dates from all tasks in the project and
+# sort the list, so that we get a time line with the future X coordinates
+# in the diagram.
+# Then we define for each timeline "segment" the "inclination" of the curve,
+# as defined by the resource consumption over time.
+# As a result, we can draw the planned_value curve and compare it with 
+# values from the past.
+
+set planned_value_sql "
+	select	p.*,
+		CASE WHEN duration_hours = 0 THEN 0 ELSE coalesce(planned_units, 0.0) / duration_hours END as inclination
+	from	(
+			select	p.tree_sortkey,
+				p.project_id,
+				p.project_nr,
+				extract(epoch from p.start_date) as start_epoch,
+				extract(epoch from p.end_date) as end_epoch,
+				(extract(epoch from coalesce(p.end_date,p.start_date)) - extract(epoch from p.start_date)) / 3600.0 as duration_hours,
+				coalesce(t.planned_units, 0.0) as planned_units,
+				t.uom_id
+			from	im_projects main_p,
+				im_projects p
+				LEFT OUTER JOIN im_timesheet_tasks t ON (p.project_id = t.task_id)
+			where	main_p.project_id = :diagram_project_id and
+				p.tree_sortkey between main_p.tree_sortkey and tree_right(main_p.tree_sortkey) and
+				p.start_date is not null
+		) p
+	order by p.tree_sortkey
+"
+# Write the results into a multirow, because we'll need it twice
+db_multirow mr planned_value_sql $planned_value_sql
+
+
+# First pass: Collect all start and end points of project tasks
+set total_planned_value 0.0
+template::multirow foreach mr {
+    set timeline_hash($start_epoch) 1
+    set timeline_hash($end_epoch) 1
+    set total_planned_value [expr $total_planned_value + $planned_units]
+}
+
+set timeline_list [lsort -integer [array name timeline_hash]]
+
+# Create a reverted index on the timeline_list, so that we can 
+# find the index for each epoch quickly. This will reduce the
+# complexity of the algorithm to N*log(N)
+set ctr 0
+foreach epoch $timeline_list {
+    set epoch_hash($epoch) $ctr
+    incr ctr
+}
+
+# Second pass: Sum up the inclination for each segment
+array set inclination_hash {}
+template::multirow foreach mr {
+    set start_idx $epoch_hash($start_epoch)
+    set end_idx $epoch_hash($end_epoch)
+    
+    ns_log Notice "project-eva: start_idx=$start_idx, end_idx=$end_idx, inclination=$inclination"
+
+    for {set i $start_idx} {$i < $end_idx} {incr i} {
+	set incl 0.0
+	if {[info exists inclination_hash($i)]} {
+	    set incl $inclination_hash($i)
+	}
+	set incl [expr $incl + $inclination]
+	set inclination_hash($i) $incl
+    }
+}
+
+# ad_return_complaint 1 [array get inclination_hash]
+
+# ----------------------------------------------------
+# Integrate the graph up
+# ----------------------------------------------------
+
+
+set timeline_list_len [expr [llength $timeline_list] - 1]
+set value 0.0
+array set planned_value_hash {}
+set planned_value_hash(0) $value
+for {set i 0} {$i < $timeline_list_len} {incr i} {
+    set start_epoch [lindex $timeline_list $i]
+    set end_epoch [lindex $timeline_list [expr $i+1]]
+
+    set inclination 0.0
+    if {[info exists inclination_hash($i)]} { set inclination $inclination_hash($i) }
+    set duration_hours [expr ($end_epoch - $start_epoch) / 3600.0]
+    set delta [expr $duration_hours * $inclination]
+    set value [expr $value + $delta]
+
+    ns_log Notice "project-eva: i=$i: start:[im_date_epoch_to_ansi $start_epoch] [im_date_epoch_to_time $start_epoch], end:[im_date_epoch_to_ansi $end_epoch] [im_date_epoch_to_time $end_epoch], duration_hours=$duration_hours, inclination=$inclination, delta=$delta, value=$value"
+    set planned_value_hash([expr $i+1]) $value
+}
+
+
+# ----------------------------------------------------
+# Show Financial documents during intervals
+# ----------------------------------------------------
+
+set cost_type_ids [list [im_cost_type_invoice] [im_cost_type_quote] [im_cost_type_order] [im_cost_type_timesheet]]
+set timesheet_sql "
+	select	main_p.project_budget,
+		main_p.project_budget_hours,
+		extract(epoch from c.effective_date) as effective_epoch,
+		c.*
+	from	im_projects main_p,
+		im_projects p,
+		im_costs c
+	where	main_p.project_id = :diagram_project_id and
+		p.tree_sortkey between main_p.tree_sortkey and tree_right(main_p.tree_sortkey) and
+		c.project_id = p.project_id and
+		c.cost_type_id in ([join $cost_type_ids ", "])
+	order by
+		c.cost_type_id,
+		c.effective_date
+"
+set old_cost_type_id 0
+array set cost_hash {}
+db_foreach ts $timesheet_sql {
+
+    # Initiate the index to the timeline
+    if {$cost_type_id != $old_cost_type_id} {
+	set ctr 0
+	set ctr_epoch [lindex $timeline_list $ctr]
+	set old_cost_type_id $cost_type_id
+    }
+
+    ns_log Notice "project-eva.json: ctr=$ctr, cost_type_id=$cost_type_id, amount=$amount"
+
+    # Calculate the timeline slot for the cost item
+    while {$effective_epoch > $ctr_epoch && $ctr < $timeline_list_len} {
+	incr ctr
+	set ctr_epoch [lindex $timeline_list $ctr]
+    }
+
+    # Update the hash
+    set key "$cost_type_id-$ctr"
+    set value 0.0
+    if {[info exists cost_hash($key)]} { set value $cost_hash($key) }
+    set value [expr $value + $amount]
+    set cost_hash($key) $value
+}
+
+# ----------------------------------------------------
+# Create JSON for data source
+# ----------------------------------------------------
+
+set ctr 0
+set json_lines {}
+foreach epoch $timeline_list {
+    set planned_value [expr round(100.0 * $planned_value_hash($ctr)) / 100.0]
+
+    set json_values [list]
+    lappend json_values "'date': '[im_date_epoch_to_ansi $epoch] [im_date_epoch_to_time $epoch]'"
+    lappend json_values "'planned_value': $planned_value"
+    lappend json_values "'total_planned_value': $total_planned_value"
+
+    foreach cost_type_id $cost_type_ids {
+	set key "$cost_type_id-$ctr"
+	set value 0.0
+	if {[info exists cost_hash($key)]} { set value $cost_hash($key) }
+	lappend json_values "'cost_type_$cost_type_id': $value"
+    }
+
+    lappend json_lines "{[join $json_values ", "]}"
+    incr ctr
+}
+
+set json "{\"success\": true, \"message\": \"Data loaded\", \"data\": \[\n[join $json_lines ",\n"]\n\]}"
+doc_return 200 "text/html" $json
+
+
