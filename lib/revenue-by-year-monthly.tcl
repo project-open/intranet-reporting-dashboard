b2e8d2f38434f982d196523bdf0a9dc5e5fd2992
Project Open <info@project-open.com>
2021-02-19 18:00:50 +0100

    http -> httpd

--- lib/revenue-by-year-monthly.tcl
+++ lib/revenue-by-year-monthly.tcl
@@ -3,7 +3,7 @@
 # Copyright (C) 2019 ]project-open[
 #
 # All rights reserved. Please check
-# http://www.project-open.com/license/ for details.
+# https://www.project-open.com/license/ for details.
 
 # ----------------------------------------------------------------------
 # Variables
